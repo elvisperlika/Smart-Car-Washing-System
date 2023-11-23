cf3a6a0068fd9d4cfcfb9fb663a2f25a6a7fafce
Enea <e.bresciani@cosmobile.com>
2023-11-23 22:40:25 +0100

    Added component tempsensor tmp36

---
+++ src/components/TempSensorTMP36.cpp
@@ -0,0 +1,14 @@
+#include "TempSensorLM35.h"
+#include "Arduino.h"
+
+#define VOLT_FACTOR ((float)0.0048828145)
+
+TempSensorLM35::TempSensorLM35(int p) : pin(p){
+}
+
+float TempSensorLM35::getTemperature(){
+    int value = analogRead(pin);
+    float voltage = value * VOLT_FACTOR;
+    float valueInCelsius = (voltage - 0.5) * 100.0;
+    return valueInCelsius;
+}
