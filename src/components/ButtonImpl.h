feaa04bc75e8d358357ad42f4131f79173d0a234
Matteo Morellini <matteomorellini92@gmail.com>
2023-11-25 11:51:49 +0100

    Comments Added

--- src/components/ButtonImpl.h
+++ src/components/ButtonImpl.h
@@ -1,18 +1,18 @@
-#ifndef __BUTTONIMPL__
+#ifndef __BUTTONIMPL__ // Header guard to prevent multiple inclusion of this header file
 #define __BUTTONIMPL__
 
-#include "Button.h"
+#include "Button.h" // Include the Button class header file
 
-class ButtonImpl: public Button {
+class ButtonImpl : public Button { // ButtonImpl class inherits from the Button class
  
 public: 
-  ButtonImpl(int pin);
+  ButtonImpl(int pin); // Constructor for ButtonImpl class, taking a pin as a parameter
 
-  bool isClicked();
+  bool isClicked(); // Overrides the isClicked() method from the base class Button
 
 private:
-  int pin;
-  bool clicked;
+  int pin;      // Member variable to hold the pin number associated with the button
+  bool clicked; // Member variable to track the click state of the button
 };
 
-#endif
+#endif // End of the header guard
