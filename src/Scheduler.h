5c884d3962b5a0655c518d80decb3199e64c2fd1
IGieckI <giacomo.antonelli3@gmail.com>
2023-11-27 14:15:15 +0100

    Scheduler refactory

--- src/Scheduler.h
+++ src/Scheduler.h
@@ -3,19 +3,17 @@
 
 #include "tasks/Task.h"
 
-#define MAX_TASKS 50
+#define MAX_TASKS 20
 
 class Scheduler {
-  
-  int basePeriod;
-  int nTasks;
-  Task* taskList[MAX_TASKS];  
+    int basePeriod;
+    int nTasks;
+    Task* taskList[MAX_TASKS];  
 
-  public:
-  void init(int basePeriod);  
-  virtual bool addTask(Task* task);  
-  virtual void schedule();
-
+    public:
+    void init(int basePeriod);  
+    virtual bool addTask(Task* task);  
+    virtual void schedule();
 };
 
 #endif
