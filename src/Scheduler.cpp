432e91f93fbd7501c9e068b0d02711208255cc85
Elvis Perlika <elvis>
2023-11-29 14:51:26 +0100

    fix scheduler bug about adding new task

--- src/Scheduler.cpp
+++ src/Scheduler.cpp
@@ -7,8 +7,6 @@ Timer* Timer_1;
 void Scheduler::init(int basePeriod){
     Timer_1 = new Timer();
     this->basePeriod = basePeriod;
-    long period = 1000l*basePeriod;
-    
     Timer_1->setupPeriod(basePeriod);
     nTasks = 0;
 }
@@ -16,8 +14,8 @@ void Scheduler::init(int basePeriod){
 bool Scheduler::addTask(Task* task){
     delay(500);
     if (nTasks < MAX_TASKS-1){
-        taskList[nTasks] = task;
-        nTasks++;
+        this->taskList[nTasks] = task;
+        this->nTasks++;
         return true;
     } else {
         return false; 
@@ -25,10 +23,9 @@ bool Scheduler::addTask(Task* task){
 }
   
 void Scheduler::schedule(){   
-
     Timer_1->waitForNextTick();
 
-    for (int i = 0; i <= nTasks; i++){
+    for (int i = 0; i < nTasks; i++){
         if (taskList[i]->isActive() && taskList[i]->updateAndCheckTime(basePeriod)){
             taskList[i]->tick();
         }
--- src/main.cpp
+++ src/main.cpp
@@ -2,25 +2,33 @@
 #include <Scheduler.h>
 #include "tasks/tempCheckTask/TempCheckTask.h"
 #include "tasks/stateTask/StateTask.h"
+#include "tasks/detectMotionTask/DetectionTask.h"
 
 Scheduler sched;
+CarWash *carWash;
+
 Task *accessTask;
 Task *stateTask;
-CarWash *carWash;
+Task *detectMotionTask;
 
 void setup()
 {
+    sched.init(100);
+
     carWash = new CarWash();
     accessTask = new TempCheckTask(100, carWash);
     // sched.addTask(accessTask);
 
+    detectMotionTask = new DetectionTask(100, carWash);
+    detectMotionTask->setName("DetectionTask");
+    detectMotionTask->setActive(true);
+
     stateTask = new StateTask(1000, carWash);
     stateTask->setName("StateTask");
     stateTask->setActive(true);
-    sched.addTask(stateTask);
 
-    sched.init(100);
-    Serial.begin(9600);
+    sched.addTask(detectMotionTask);
+    sched.addTask(stateTask);
 }
 
 void loop()
