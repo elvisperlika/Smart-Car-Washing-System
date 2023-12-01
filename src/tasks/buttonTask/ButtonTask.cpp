deb05a6eef8b66934767af75b30d38e884f7a77b
IGieckI <giacomo.antonelli3@gmail.om>
2023-12-01 13:34:14 +0100

    Fix on ButtonTask FSM

--- src/tasks/buttonTask/ButtonTask.cpp
+++ src/tasks/buttonTask/ButtonTask.cpp
@@ -2,8 +2,19 @@
 
 ButtonTask::ButtonTask(int period, CarWash *carWash) : Task(period, carWash) { }
 
-void ButtonTask::checkButtonState() {
-    if (carWash->getState() == READY_TO_BE_WASHED && carWash->isButtonPressed()) {
-        carWash->setState(READY_TO_BE_WASHED_BUTTON_PRESSED);
+void ButtonTask::tick() {
+    switch (state)
+    {
+        case ButtonTaskStates::OFFLINE:
+            if (carWash->getState() == SystemState::READY_TO_BE_WASHED && carWash->isButtonPressed()) {
+                state = ButtonTaskStates::BUTTON_PRESSED;
+            }
+            break;
+        case ButtonTaskStates::BUTTON_PRESSED:
+            if (!carWash->isButtonPressed()) {
+                carWash->setState(SystemState::READY_TO_BE_WASHED_BUTTON_PRESSED);
+                state = ButtonTaskStates::OFFLINE;
+            }
+            break;
     }
 }
